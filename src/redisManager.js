5490c812824f00cd44dff146d2b436e411b1c63f
shirfr-ma <shir.fr@moonactive.com>
2022-03-24 11:27:57 +0200

    returning links on every response

--- src/redisManager.js
+++ src/redisManager.js
@@ -8,18 +8,20 @@ const start = async () => {
 const addLink = async (userId, link, name, keywords, comment) => {
     const data = {name, keywords, comment};
     await client.HSET(`links:${userId}`, link, JSON.stringify(data));
+    return await getAllLinks(userId);
 };
 
 const getAllLinks = async (userId) => {
     return await client.HGETALL(`links:${userId}`);
 };
 
-const getLink = async (link) => {
-    return await client.HGET('links:1', link);
+const getLink = async (userId, link) => {
+    return await client.HGET(`links:${userId}`, link);
 };
 
 const removeLink = async (userId, link) => {
     await client.HDEL(`links:${userId}`, link);
+    return await getAllLinks(userId);
 };
 
 const removeAllLinks = async (userId) => {
--- src/routes/links.js
+++ src/routes/links.js
@@ -14,31 +14,46 @@ router.use('/hello', (req, res) => {
     res.send({message: "hello"});
 });
 
-router.post('/saveLink/:id', (req, res) => {
-    let {keywords, link, name, comment} = req.body;
-    const userId = req.params.id;
-    let keywordsSplit = keywords.split(",");
-    redisManager.addLink(userId, link, name, keywordsSplit, comment);
-    setHeaders(res);
-    console.log("saved link");
-    res.send({message: "Link saved successfully"});
+router.post('/saveLink/:id', async (req, res) => {
+    try {
+        let {keywords, link, name, comment} = req.body;
+        const userId = req.params.id;
+        let keywordsSplit = keywords.split(",");
+        const links = await redisManager.addLink(userId, link, name, keywordsSplit, comment);
+        setHeaders(res);
+        console.log("saved link");
+        res.send({links});
+    } catch (error) {
+        res.statusCode(500);
+        res.send({message: "Failed to save link", error});
+    }
 });
 
-router.use('/removeLink/:id', (req, res) => {
-    setHeaders(res);
-    let {url} = req.body;
-    const userId = req.params.id;
-    redisManager.removeLink(userId, url);
-    console.log("removed link");
-    res.send({message: "Link removed successfully"});
+router.use('/removeLink/:id', async (req, res) => {
+    try {
+        setHeaders(res);
+        let {url} = req.body;
+        const userId = req.params.id;
+        const links = await redisManager.removeLink(userId, url);
+        console.log("removed link");
+        res.send({links});
+    } catch (error) {
+        res.statusCode(500);
+        res.send({message: "Failed to remove link", error});
+    }
 });
 
-router.use('/removeAllLinks/:id', (req, res) => {
-    setHeaders(res);
-    const userId = req.params.id;
-    redisManager.removeAllLinks(userId);
-    console.log("removed all links");
-    res.send({message: "All links removed successfully"});
+router.use('/removeAllLinks/:id', async (req, res) => {
+    try {
+        setHeaders(res);
+        const userId = req.params.id;
+        await redisManager.removeAllLinks(userId);
+        console.log("removed all links");
+        res.send({message: "All links removed successfully"});
+    } catch (error) {
+        res.statusCode(500);
+        res.send({message: "Failed to remove all links", error});
+    }
 });
 
 router.use('/getAllLinks/:id', async (req, res) => {
