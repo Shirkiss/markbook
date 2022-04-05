c3d6be185502b3505d16abd44b4f5f0c4ce264d6
shirfr-ma <shir.fr@moonactive.com>
2022-04-05 10:55:29 +0300

    adding elasticsearch service

---
+++ src/services/elasticsearch.js
@@ -0,0 +1,155 @@
+const {Client} = require('@elastic/elasticsearch');
+
+class ElasticSearch {
+    constructor(index) {
+        this.client = new Client({
+            cloud: {
+                id: 'markbook:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDNhY2VkZjkyYjkyOTQ0Yzg4NGMzZDI3NDUzZWMwNGJiJGM1MTM0NjU2ZDk4ODQyYjY4MmU0OGZjMjRmNjIzYzc0'
+            },
+            auth: {
+                apiKey: 'M3FvUTRIOEJCZ1RXNFR5UUlmOXE6Vlk4R25EME5UWHVVSWwxXzBCOEpFUQ==',
+            },
+        });
+        this.index = index;
+    }
+
+    async init() {
+        await this.createIndex(this.index);
+    }
+
+    async createIndex(index) {
+        const exists = await this.client.indices.exists({
+            index
+        });
+        if (!exists) {
+            await this.client.indices.create({
+                index,
+            })
+        }
+    }
+
+    async deleteIndex() {
+        await this.client.indices.delete({index: this.index});
+    }
+
+    async deleteById(id) {
+        await this.client.delete({index: this.index, id});
+    }
+
+    async addLink(name, urlValue, keywords, caption, userId) {
+        await this.client.index({
+            index: this.index,
+            document: {name, urlValue, keywords, caption, userId}
+        })
+        await this.client.indices.refresh({index: this.index})
+    }
+
+
+    async prefixSearch(prefix, field, userId) {
+        const result = await this.client.search({
+            index: this.index,
+            body: {
+                query: {
+                    "bool": {
+                        "must": [{
+                            "term": {userId}
+                        }, {
+                            "match_phrase_prefix": {
+                                [field]: {
+                                    "query": prefix,
+                                    "max_expansions": 10
+                                }
+                            }
+                        }],
+                    },
+                }
+            }
+        })
+        return result.hits.hits;
+    }
+
+
+    async prefixSearchMulti(prefix, fields, userId) {
+        const result = await this.client.search({
+                index: this.index,
+                body: {
+                    query: {
+                        "bool": {
+                            "must": [{
+                                "term": {userId}
+                            }, {
+                                multi_match: {
+                                    query: prefix,
+                                    type: 'phrase_prefix',
+                                    fields
+                                },
+                            }],
+                        },
+                    }
+                }
+            }
+        )
+        return result.hits.hits;
+    }
+
+    async prefixSearchAllFields(prefix, userId) {
+        const result = await this.client.search({
+                index: this.index,
+                body: {
+                    query: {
+                        "bool": {
+                            "must": [{
+                                "term": {userId}
+                            }, {
+                                multi_match: {
+                                    query: prefix,
+                                    type: 'phrase_prefix',
+                                },
+                            }],
+                        },
+                    }
+                }
+            }
+        );
+        return result.hits.hits;
+
+    }
+
+
+    async mostFrequentKeyword(limit) {
+        const result = await this.client.search({
+            index: this.index,
+            body: {
+                aggs: {
+                    top_keywords: {
+                        terms: {
+                            field: 'keywords.keyword',
+                            size: limit
+                        },
+                    }
+                }
+            }
+        });
+        return result.aggregations.top_keywords.buckets;
+    }
+
+    async mostFrequentKeywordForUser(userId, limit) {
+        const result = await this.client.search({
+            index: this.index,
+            body: {
+                "query": {"term": {userId}},
+                aggs: {
+                    top_keywords: {
+                        terms: {
+                            field: 'keywords.keyword',
+                            size: limit
+                        },
+                    }
+                }
+            }
+        });
+        return result.aggregations.top_keywords.buckets;
+    }
+}
+
+module.exports = ElasticSearch;
