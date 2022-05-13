0172ed48cf2402d8ad36881379c93141e6f2cefb
shirfr-ma <shir.fr@moonactive.com>
2022-05-13 16:52:44 +0300

    small changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import * as FaIcons from 'react-icons/fa'
 import styled from 'styled-components'
 
 
-import { SidebarData } from './SidebarData';
+import {SidebarData} from './SidebarData';
 
 const Navbar = styled.div`
   display: flex;
@@ -15,68 +15,71 @@ const Navbar = styled.div`
 `
 
 const MenuIconOpen = styled.div`
-    display: flex;
-    justify-content: start;
-    font-size: 1.5rem;
-    margin-left: 2rem;
-    color: #ffffff;
+  display: flex;
+  justify-content: start;
+  font-size: 1.5rem;
+  margin-left: 2rem;
+  color: #ffffff;
 `
 
 const MenuIconClose = styled.div`
-    display: flex;
-    justify-content: end;
-    font-size: 1.5rem;
-    margin-top: 0.75rem;
-    margin-right: 1rem;
-    color: #ffffff;
+  display: flex;
+  justify-content: end;
+  font-size: 1.5rem;
+  margin-top: 0.75rem;
+  margin-right: 1rem;
+  color: #ffffff;
 `
 
-const SidebarMenu = styled.div<{close: boolean}>`
-    width: 50px;
-    height: 100vh;
+const SidebarMenu = styled.div<{ close: boolean }>`
+  width: 50px;
+  height: 100vh;
   background-color: #232345;
-    position: fixed;
-    top: 0;
-    right: ${({ close}) => close ? '-100%' : '0'};
-    transition: .6s;
+  position: fixed;
+  top: 0;
+  right: ${({close}) => close ? '-100%' : '0'};
+  transition: .6s;
 `
 
 const MenuItems = styled.li`
-   list-style: none;
-   display: flex;
-   -webkit-box-align: center;
-   align-items: center;
-   -webkit-box-pack: start;
-   justify-content: start;
-   width: 100%;
-   height: 15px;
-   padding: 1rem 0px 1.25rem;
+  list-style: none;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: start;
+  justify-content: start;
+  width: 100%;
+  height: 15px;
+  padding: 1rem 0px 1.25rem;
 `
 
 const ArrowSelected = styled.div`
-    width: 0;
-    height: 0;
-    border-top: 8px solid transparent;
-    border-bottom: 8px solid transparent;
-    border-left: 8px solid white;
+  width: 0;
+  height: 0;
+  border-top: 8px solid transparent;
+  border-bottom: 8px solid transparent;
+  border-left: 8px solid white;
 `
 
 const MenuItemDiv = styled.div`
-    display: flex;
-    align-items: center;
-    margin: auto;
-    font-size: 20px;
-    text-decoration: none;
-    color: #ffffff;
+  display: flex;
+  align-items: center;
+  margin: auto;
+  font-size: 20px;
+  text-decoration: none;
+  color: #ffffff;
 `
 
-const Sidebar: React.FunctionComponent<{setCurrentTab:Function, currentTab: string}> = ({setCurrentTab, currentTab = 'Tabs'}) => {
+const Sidebar: React.FunctionComponent<{ setCurrentTab: Function, currentTab: string }> = ({
+                                                                                               setCurrentTab,
+                                                                                               currentTab = 'Tabs'
+                                                                                           }) => {
     const [close, setClose] = useState(false)
     const showSidebar = () => setClose(!close)
 
-   const handleMenuClick = (title: string) => {
+    const handleMenuClick = (title: string) => {
         setCurrentTab(title);
-   }
+    }
 
     return (
         <>
@@ -84,7 +87,7 @@ const Sidebar: React.FunctionComponent<{setCurrentTab:Function, currentTab: stri
                 {SidebarData.map((item, index) => {
                     return (
                         <MenuItems key={index}>
-                            {currentTab === item.title && <ArrowSelected />}
+                            {currentTab === item.title && <ArrowSelected/>}
                             <MenuItemDiv onClick={() => handleMenuClick(item.title)}>
                                 {item.icon}
                             </MenuItemDiv>
